b9e595b2f80b0d880dcb0c22fa0f916c1dfea387
Frederick Akalin <akalin@keyba.se>
2016-06-13 18:35:22 -0700

    Split MD memory/disk

--- libkbfs/interfaces.go
+++ libkbfs/interfaces.go
@@ -944,6 +944,14 @@ type MDServer interface {
 		BareTlfHandle, error)
 }
 
+type mdServerLocal interface {
+	MDServer
+	addNewAssertionForTest(
+		uid keybase1.UID, newAssertion keybase1.SocialAssertion) error
+	isShutdown() bool
+	copy(config Config) mdServerLocal
+}
+
 // BlockServer gets and puts opaque data blocks.  The instantiation
 // should be able to fetch session/user details via KBPKI.  On a
 // put/delete, the server is reponsible for: 1) checking that the ID
